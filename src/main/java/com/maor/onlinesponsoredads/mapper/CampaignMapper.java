46a5527d9f39c82e66f6a1040a81d82f99aa6943
Maorazr <maoraz@post.bgu.ac.il>
2023-12-17 22:59:25 +0200

    Update CampaignMapper product mapping

--- src/main/java/com/maor/onlinesponsoredads/mapper/CampaignMapper.java
+++ src/main/java/com/maor/onlinesponsoredads/mapper/CampaignMapper.java
@@ -2,14 +2,43 @@ package com.maor.onlinesponsoredads.mapper;
 
 import com.maor.onlinesponsoredads.dto.CampaignDto;
 import com.maor.onlinesponsoredads.model.Campaign;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import com.maor.onlinesponsoredads.model.Product;
+import com.maor.onlinesponsoredads.repository.ProductRepository;
+import java.util.List;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ProductMapper.class)
 public interface CampaignMapper {
-  @Mapping(target = "productsSerialNumbers", source = "products")
-  CampaignDto campaignToDto(Campaign campaign);
+  @Mapping(source = "products", target = "productsSerialNumbers")
+  CampaignDto campaignToDto(
+    Campaign campaign,
+    @Context ProductRepository productRepository
+  );
 
-  @Mapping(target = "products", source = "productsSerialNumbers")
-  Campaign dtoToCampaign(CampaignDto dto);
+  @Mapping(source = "productsSerialNumbers", target = "products")
+  @Mapping(target = "id", ignore = true)
+  Campaign dtoToCampaign(
+    CampaignDto campaignDto,
+    @Context ProductRepository productRepository
+  );
+
+  default List<Product> mapListOfSerialToProducts(
+    List<Long> serials,
+    @Context ProductRepository productRepository
+  ) {
+    if (serials == null) {
+      return null;
+    }
+    return productRepository.findAllById(serials);
+  }
+
+  default Product mapSerialNumberToProduct(
+    Long serialNumber,
+    @Context ProductRepository productRepository
+  ) {
+    if (serialNumber == null) {
+      return null;
+    }
+    return productRepository.findById(serialNumber).orElse(null);
+  }
 }
