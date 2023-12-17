d80e4421d70d7adbe09642b549f0ec81671c58a3
Maorazr <maoraz@post.bgu.ac.il>
2023-12-17 23:14:01 +0200

    Added product query by campaign date and category

--- src/main/java/com/maor/onlinesponsoredads/model/Campaign.java
+++ src/main/java/com/maor/onlinesponsoredads/model/Campaign.java
@@ -1,22 +1,13 @@
 package com.maor.onlinesponsoredads.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
--- src/main/java/com/maor/onlinesponsoredads/repository/ProductRepository.java
+++ src/main/java/com/maor/onlinesponsoredads/repository/ProductRepository.java
@@ -1,8 +1,15 @@
 package com.maor.onlinesponsoredads.repository;
 
 import com.maor.onlinesponsoredads.model.Product;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  List<Product> findByCampaignsStartDateLessThanEqualAndCategory(
+    LocalDateTime date,
+    String category
+  );
+}
